4d38c54ee6e6ee332761575fa5cd93a0985fa260
Simo Kivimäki <kivimaki@a88-115-199-134.elisa-laajakaista.fi>
2010-01-26 19:38:37 +0200

    Added main method and test.

---
+++ test/org/agilefinland/coffeemaker/CoffeeMakerTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.agilefinland.coffeemaker;
+
+import org.junit.Test;
+
+
+/**
+ * @author kivimaki
+ *
+ */
+public class CoffeeMakerTest {
+
+	
+	@Test
+	public void testMain() {
+		CoffeeMaker.main(null);
+	}
+}
