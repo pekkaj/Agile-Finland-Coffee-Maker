23bf30c9d14e5b3dfd7db7d1a69636c2d905e2f9
Simo Kivimäki <kivimaki@a88-115-200-254.elisa-laajakaista.fi>
2010-02-23 19:36:55 +0200

    Added test skeleton for CoffeeMakerObserverTest.

---
+++ test/org/agilefinland/coffeemaker/CoffeeMakerObserverTest.java
@@ -0,0 +1,20 @@
+package org.agilefinland.coffeemaker;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class CoffeeMakerObserverTest {
+
+	@Test
+	public void testSetReliefValveOn() {
+		Assert.fail();
+	}
+	
+	@Test
+	public void testSetBoilerPlateOn() {
+		Assert.fail();
+	}
+	
+	
+}
