143dbcdebb70b9ce98cf8d33d70806e749732d88
unknown <mimp@.(none)>
2010-03-09 19:00:24 +0200

    After the brew button is pressed turn on the heating

--- src/org/agilefinland/coffeemaker/CoffeeMakerObserver.java
+++ src/org/agilefinland/coffeemaker/CoffeeMakerObserver.java
@@ -3,16 +3,17 @@ package org.agilefinland.coffeemaker;
 public class CoffeeMakerObserver implements Observer {
 
 	private final CoffeeMakerAPI hardware;
-	
+
 	public CoffeeMakerObserver(CoffeeMakerAPI hdw) {
-		
+
 		hardware = hdw;
 	}
-	
+
 	@Override
 	public void handleEvent(PollEvent event) {
 		if(PollEvent.BUTTON_PRESSED.equals(event)) {
 			hardware.setIndicatorState(CoffeeMakerAPI.INDICATOR_ON);
+			hardware.setBoilerState(CoffeeMakerAPI.BOILER_ON);
 		}
 	}
 
--- test/org/agilefinland/coffeemaker/CoffeeMakerObserverTest.java
+++ test/org/agilefinland/coffeemaker/CoffeeMakerObserverTest.java
@@ -3,6 +3,7 @@ package org.agilefinland.coffeemaker;
 import junit.framework.Assert;
 
 import org.easymock.EasyMock;
+import org.hamcrest.core.AnyOf;
 import org.junit.Test;
 
 public class CoffeeMakerObserverTest {
@@ -10,34 +11,36 @@ public class CoffeeMakerObserverTest {
 	@Test
 	public void testSetReliefValveOn() {
 		CoffeeMakerAPI hwMock = EasyMock.createMock(CoffeeMakerAPI.class);
-		
+
 		// EasyMock.expect(hwMock.set)
-		
+
 		CoffeeMakerObserver observer = new CoffeeMakerObserver(hwMock);
-		
+
 		// FIXME ...
 	}
-	
+
 	@Test
 	public void testSetBoilerOn() {
 		CoffeeMakerAPI hwMock = EasyMock.createMock(CoffeeMakerAPI.class);
 
-		hwMock.setBoilerState(CoffeeMakerAPI.BOILER_ON);
-		EasyMock.expectLastCall();
-		
+		hwMock.setIndicatorState( EasyMock.anyInt());
+		hwMock.setBoilerState(EasyMock.eq(CoffeeMakerAPI.BOILER_ON));
+
+		EasyMock.replay(hwMock);
+
 		CoffeeMakerObserver observer = new CoffeeMakerObserver(hwMock);
 		observer.handleEvent(PollEvent.BUTTON_PRESSED);
-		
+
 		EasyMock.verify(hwMock);
 	}
-	
+
 	@Test
 	public void testSettingOnTheIndicatorWhenBrewButtonIsPressed(){
-		
+
 		CoffeeMakerAPI coffeeMakerAPI = new SysoutCoffeeMakerAPI();
 		Observer makerObserver = new CoffeeMakerObserver(coffeeMakerAPI);
 		makerObserver.handleEvent(PollEvent.BUTTON_PRESSED);
 		Assert.assertEquals(coffeeMakerAPI.getIndicatorState(), CoffeeMakerAPI.INDICATOR_ON);
-		
+
 	}
 }
